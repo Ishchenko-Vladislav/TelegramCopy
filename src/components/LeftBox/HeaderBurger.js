b8e7935b8a483c4d9d9d1a58dd78189f49c3dad3
vladislav <patriq.968@gmail.com>
2022-10-26 17:47:23 +0300

    l

--- src/components/LeftBox/HeaderBurger.js
+++ src/components/LeftBox/HeaderBurger.js
@@ -15,89 +15,97 @@ import { ReactComponent as WIcon } from "../../images/w.svg";
 
 export const HeaderBurger = () => {
   const [showPopUp, setShowPopUp] = useState(false);
+  const [isClosing, setClosing] = useState(false);
   const { themeMode, setThemeMode } = useContext(ThemeContext);
-  const ClickOnBlur = (e) => {
-    console.log(e.className);
-    if ([e.className].has("header-burger__set")) {
-      setShowPopUp(false);
+  const handleCloseBackDrop = () => {
+    if (showPopUp) {
+      setClosing(true);
+      setTimeout(() => {
+        setShowPopUp(false);
+        setClosing(false);
+      }, 200);
+    } else {
+      setShowPopUp(true);
     }
-    // e.stopPropagation()
   };
-  useEffect(() => {
-    document.addEventListener("click", ClickOnBlur);
-    return () => {
-      document.removeEventListener("click", ClickOnBlur);
-    };
-  }, []);
+  const closeBackDrop = isClosing ? "backDrop backDrop-hide" : "backDrop";
+  const burgerClasses = showPopUp
+    ? `.header-burger__set active ${themeMode && "dark"}`
+    : `.header-burger__set ${themeMode && "dark"}`;
 
   return (
-    <div onClick={(e) => ClickOnBlur(e.target)} className="header-burger">
-      <div onClick={() => setShowPopUp(!showPopUp)} className={`header-burger__set ${themeMode && "dark"}`}>
+    <div onClick={() => {}} className="header-burger">
+      <div onClick={handleCloseBackDrop} className={burgerClasses}>
         <div className={`leftBox__header-burger ${themeMode && "dark"}`}>
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
-
-      <div className={showPopUp ? `pop-up__burger active ${themeMode && "dark"}` : `pop-up__burger ${themeMode && "dark"}`}>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} height={25} src={bannerIcon} alt="bannerIcon" /> */}
-          <BannerIcon width={25} />
-          <div>Saved Message</div>
-        </button>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} src={archiveIcon} alt="archiveIcon" /> <div>Archived Chats</div> */}
-          <ArchiveIcon width={25} />
-          <div>Archived Chats</div>
-        </button>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} src={contactIcon} alt="contactIcon" /> */}
-          <ContactIcon width={25} />
-          <div>Contacts</div>
-        </button>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} src={settingIcon} alt="settingIcon" /> */}
-          <SettingIcon width={25} />
-          <div>Settings</div>
-        </button>
-        <button onClick={(e) => setThemeMode(!themeMode)} className="pop-up__burger-item nightMode">
-          <div style={{ pointerEvents: "none" }} className="nightMode-item">
-            {/* <img width={25} src={nightModeIcon} alt="nightModeIcon" /> */}
-            <NightModeIcon width={25} />
-            <div>Night Mode</div>
+      {showPopUp ? (
+        <div onClick={handleCloseBackDrop} className={closeBackDrop}>
+          <div onClick={(event) => event.stopPropagation()} className={`pop-up__burger  ${themeMode && "dark"}`}>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} height={25} src={bannerIcon} alt="bannerIcon" /> */}
+              <BannerIcon width={25} />
+              <div>Saved Message</div>
+            </button>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} src={archiveIcon} alt="archiveIcon" /> <div>Archived Chats</div> */}
+              <ArchiveIcon width={25} />
+              <div>Archived Chats</div>
+            </button>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} src={contactIcon} alt="contactIcon" /> */}
+              <ContactIcon width={25} />
+              <div>Contacts</div>
+            </button>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} src={settingIcon} alt="settingIcon" /> */}
+              <SettingIcon width={25} />
+              <div>Settings</div>
+            </button>
+            <button onClick={(e) => setThemeMode(!themeMode)} className="pop-up__burger-item nightMode">
+              <div style={{ pointerEvents: "none" }} className="nightMode-item">
+                {/* <img width={25} src={nightModeIcon} alt="nightModeIcon" /> */}
+                <NightModeIcon width={25} />
+                <div>Night Mode</div>
+              </div>
+              <label style={{ pointerEvents: "none" }} onClick={(e) => {}} class="switch">
+                <input checked={themeMode} type="checkbox" />
+                <span class="slider round"></span>
+              </label>
+            </button>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} src={infoIcon} alt="infoIcon" /> */}
+              <InfoIcon width={25} />
+              <div>Telegram Features</div>
+            </button>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} src={reportIcon} alt="reportIcon" /> */}
+              <ReportIcon width={25} />
+              <div>Report Bug</div>
+            </button>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} src={kIcon} alt="kIcon" /> */}
+              <KIcon width={25} />
+              <div>Switch to K Version</div>
+            </button>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} src={wIcon} alt="wIcon" /> */}
+              <WIcon width={25} />
+              <div>Switch to Old Version</div>
+            </button>
+            <button className="pop-up__burger-item">
+              {/* <img width={25} src={plusIcon} alt="plusIcon" /> */}
+              <PlusIcon width={25} />
+              <div>Install App</div>
+            </button>
           </div>
-          <label style={{ pointerEvents: "none" }} onClick={(e) => {}} class="switch">
-            <input checked={themeMode} type="checkbox" />
-            <span class="slider round"></span>
-          </label>
-        </button>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} src={infoIcon} alt="infoIcon" /> */}
-          <InfoIcon width={25} />
-          <div>Telegram Features</div>
-        </button>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} src={reportIcon} alt="reportIcon" /> */}
-          <ReportIcon width={25} />
-          <div>Report Bug</div>
-        </button>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} src={kIcon} alt="kIcon" /> */}
-          <KIcon width={25} />
-          <div>Switch to K Version</div>
-        </button>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} src={wIcon} alt="wIcon" /> */}
-          <WIcon width={25} />
-          <div>Switch to Old Version</div>
-        </button>
-        <button className="pop-up__burger-item">
-          {/* <img width={25} src={plusIcon} alt="plusIcon" /> */}
-          <PlusIcon width={25} />
-          <div>Install App</div>
-        </button>
-      </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
